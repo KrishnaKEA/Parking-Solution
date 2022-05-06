0a6bc42890bd63e06d5134b4a73d658ae290f1ee
KrishnaKEA <kris47f9@stud.kea.dk>
2022-05-06 18:36:29 +0200

    update

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+	import Login from "./components/Login.svelte"
+	import Home from "./components/Home.svelte"
+	import { Router, Link, Route } from 'svelte-routing';
+
+	
 </script>
+<Router>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	<div class="container">
+		<Route path="/" component={ Home } />
+		<Route path="/login" component={ Login } />
+	</div>
+</Router>
