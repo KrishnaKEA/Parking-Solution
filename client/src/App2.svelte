13d4c2fb7ab399ea478d968dec808a47c8e1f88e
Enachi Adrian <enachi.adrian52@gmail.com>
2022-05-10 11:31:30 +0200

    tailwindcss

---
+++ client/src/App2.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Login from "./components/Login2.svelte"
+	import Home from "./components/Home2.svelte"
+	import { Router, Link, Route } from 'svelte-routing';
+	import Nav from "./components/Nav.svelte";
+	
+</script>
+
+
+<main>
+<Router>
+
+	<Nav/>
+
+	<div class="container">
+		<Route path="/" component={Home} />
+		<Route path="login" component={Login} />
+	</div>
+
+	
+</Router>
+</main>
