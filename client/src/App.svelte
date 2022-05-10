bf51fb1bc492133c5b9bdc15409488c104a1d4fb
unknown <omar1807@stud.kea.dk>
2022-05-10 11:33:55 +0200

    updates files

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,13 +2,23 @@
 	import Login from "./components/Login.svelte"
 	import Home from "./components/Home.svelte"
 	import { Router, Link, Route } from 'svelte-routing';
+	
+
 
 	
 </script>
+
+
 <Router>
 
 	<div class="container">
+		
 		<Route path="/" component={ Home } />
 		<Route path="/login" component={ Login } />
 	</div>
 </Router>
+
+
+<style>
+	
+</style>
