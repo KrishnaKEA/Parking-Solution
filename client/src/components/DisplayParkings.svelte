2f88f1a36c308e1352f189282668b5963a0508b6
unknown <omar1807@stud.kea.dk>
2022-05-19 19:34:03 +0200

    fixes in displayParking component

--- client/src/components/DisplayParkings.svelte
+++ client/src/components/DisplayParkings.svelte
@@ -1,182 +1,153 @@
 <script>
-import axios from "axios";
-import { onMount } from 'svelte';
-import BookingDetails from "./BookingDetails.svelte"
-import {parkingSlot, parkingHours, BaseUrl} from "../store/parkingdata.js"
-
-
-
-  let parkings = [];
- 
-
-onMount( async() => {
-    const data = await axios.get(`${BaseUrl}/api/parkingArea`)
-    parkings = data.data.ParkingAreas;
-    console.log(parkings);
+    import axios from "axios";
+    import { onMount } from 'svelte';
+    import BookingDetails from "./BookingDetails.svelte"
+    import {parkingSlot, parkingHours, BaseUrl} from "../store/parkingdata.js"
     
     
-    parkings.map((parking)=>{
-        const parkingName = parking.name;
-        const slots = parking.slot;
+    
+      let parkings = [];
+     
+    
+    onMount( async() => {
+        const data = await axios.get(`${BaseUrl}/api/parkingArea`)
+        parkings = data.data.ParkingAreas;
+        console.log(parkings);
         
-        slots.map(async(sl)=>{
-            const slotnumber = sl.number;
-           // console.log(sl);
-            const enddate = sl.endTime;
-            if(enddate!==null){
-                const edate =new Date(enddate);
-                
-               const ndate =new Date();
-
-                if(edate < ndate){
-                    console.log("******************");
-                    console.log("FOUND!!!!");
-                    console.log("******************");
-                    const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}`)
-
-                }
-            }
+        
+        parkings.map((parking)=>{
+            const parkingName = parking.name;
+            const slots = parking.slot;
             
+            slots.map(async(sl)=>{
+                const slotnumber = sl.number;
+               // console.log(sl);
+                const enddate = sl.endTime;
+                if(enddate!==null){
+                    const edate =new Date(enddate);
+                    
+                   const ndate =new Date();
+    
+                    if(edate < ndate){
+                        console.log("******************");
+                        console.log("FOUND!!!!");
+                        console.log("******************");
+                        const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}`)
+    
+                    }
+                }
+                
+              
+    
+            })
+    
+    
+    
+    
+        //console.log(parking.slot);
+       })
+     
+    });
+    
+    
+    
+    
+    
+        
+        function changeColor() {
           
-
-        })
-
-
-
-
-    //console.log(parking.slot);
-   })
- 
-});
-
-
-
-
-
+           
+           const bgcolor = this.style.backgroundColor;
+          console.log(bgcolor);
+          this.style.backgroundColor = 'red';
+            let name = this.innerText;
+           
+            const parkingName = name.split("P");
+            const slotnumber = Number(parkingName[0]);
+            const pname = "P"+parkingName[1];
+           
+            let input = Number(prompt("please enter the number of hours in numbers"));
+        
+            while (isNaN(input)){
+        //alert("You did not enter a whole number");
+        input = prompt("Enter number");
+    }
+            let hours = Number(input);
+            hours = Math.floor(hours);
+            console.log(hours);
+          
+            this.disabled = true;
+            const detaildiv = document.querySelector(".details");
+            detaildiv.innerHTML = input + "" + name;
+            console.log(pname)
+            console.log(slotnumber)
+            console.log(hours);
     
-    function changeColor() {
-      
-       
-       const bgcolor = this.style.backgroundColor;
-      console.log(bgcolor);
-      this.style.backgroundColor = 'red';
-        let name = this.innerText;
-       
-        const parkingName = name.split("P");
-        const slotnumber = Number(parkingName[0]);
-        const pname = "P"+parkingName[1];
-       
-        let input = Number(prompt("please enter the number of hours in numbers"));
     
-        while (isNaN(input)){
-    //alert("You did not enter a whole number");
-    input = prompt("Enter number");
-}
-<<<<<<< HEAD
-
-
-
-
-    let hours = Number(input);
-    hours = Math.floor(hours);
-    console.log(hours);
     
-    this.disabled = true;
-    const detaildiv = document.querySelector(".details");
-    detaildiv.innerHTML = input + "" + name;
-    console.log(pname)
-    console.log(slotnumber)
-    console.log(hours);
-=======
-        let hours = Number(input);
-        hours = Math.floor(hours);
-        console.log(hours);
-      
-        this.disabled = true;
-        const detaildiv = document.querySelector(".details");
-        detaildiv.innerHTML = input + "" + name;
-        console.log(pname)
-        console.log(slotnumber)
-        console.log(hours);
->>>>>>> a94211d86a69badc8d39807fffbeb1374defc29f
-
-
-
-    reserveSlot(hours,pname,slotnumber);
-    resetSlot(hours,pname,slotnumber);
-
+            reserveSlot(hours,pname,slotnumber);
+           resetSlot(hours,pname,slotnumber);
+    
+        }
+        
+    async function reserveSlot(hours,parkingName,slotnumber){
+    
+     const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}/${hours}`)
+       //console.log(res);
     }
     
-async function reserveSlot(hours,parkingName,slotnumber){
-
-<<<<<<< HEAD
-    const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}/${hours}`)
-    console.log(res);
-=======
- const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}/${hours}`)
-   //console.log(res);
->>>>>>> a94211d86a69badc8d39807fffbeb1374defc29f
-}
-
-
-async function resetSlot(hours,parkingName,slotnumber){
-<<<<<<< HEAD
+    
+    async function resetSlot(hours,parkingName,slotnumber){
     setTimeout(async()=>{
         const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}`)
-    console.log(res);
-    },5000)
-=======
-setTimeout(async()=>{
-    const res =  await axios.patch(`${BaseUrl}/api/parkingarea/reservation/${slotnumber}/${parkingName}`)
-  console.log(res);
-},10000)
->>>>>>> a94211d86a69badc8d39807fffbeb1374defc29f
-
-}
-
-</script>
-
-<div class="maindiv">
-    <div class="details"></div>
-	{#each parkings as p}
-		<div id="parking">
-            {p.name}
-          
+      console.log(res);
+    },10000)
+    
+    }
+    
+    </script>
+    
+    <div class="maindiv">
+        <div class="details"></div>
+        {#each parkings as p}
+            <div id="parking">
+                {p.name}
+              
             {#each p.slot as s}
-
-
-                {#if s.isFree === true}
-                <div id="slot"><button id="btn" style="background-color: green;" on:click={changeColor}>{s.number}{p.name}</button></div>
-                {:else}
-                <div id="slot"><button id="btn" style="background-color: red;" on:click={changeColor}>{s.number}{p.name}</button></div>
-                {/if}
-
+    
+    
+            {#if s.isFree === true}
+            <div id="slot"><button id="btn" style="background-color: green;" on:click={changeColor}>{s.number}{p.name}</button></div>
+            {:else}
+            <div id="slot"><button id="btn" style="background-color: red;" on:click={changeColor}>{s.number}{p.name}</button></div>
+            {/if}
+    
             
+       
+        {/each}
+    </div>
+        {/each}
+    </div>
     
-            {/each}
-        </div>
-	{/each}
-</div>
-
-<style>
-    #slot{
-        display:flex;
-        margin-top: 10px;
-        padding:5px;
-        color:white;
-        background-color: green;
-        margin:5px;
-    }
-    #parking{
-        display:flex;
-    }
-    .maindiv{
-        position:absolute;;
-        top:50%;
-        left:50%;
-        transform: translate(-50%,-50%);
-        background-color: bisque;
-  
-    }
-    
-</style>
+    <style>
+        #slot{
+            display:flex;
+            margin-top: 10px;
+            padding:5px;
+            color:white;
+            background-color: green;
+            margin:5px;
+        }
+        #parking{
+            display:flex;
+        }
+        .maindiv{
+            position:absolute;;
+            top:50%;
+            left:50%;
+            transform: translate(-50%,-50%);
+            background-color: bisque;
+      
+        }
+        
+    </style>
