4e33c00147a3a88356f18e833665ad6e60611a1f
Enachi Adrian <enachi.adrian52@gmail.com>
2022-05-19 18:37:46 +0200

    update

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,23 +1,30 @@
 <script>
-	import { Router, Route, Link } from "svelte-routing";
-	import Home from "./components/Home.svelte";
-	import Login from "./components/Login.svelte";
-	import DisplayParkings from "./components/DisplayParkings.svelte";
-	import BookingDetails from "./components/BookingDetails.svelte";
-  
-  </script>
-  
-  <Router>
-	<nav>
-	 <Link to="/">Home</Link>
-	 <Link to="login">Login</Link>
-	 <Link to="DisplayParkings">Parkings </Link>
-	 
-	</nav>
-	<div>
-	 <Route path="/"><Home /></Route>
-	 <Route path="login" component="{Login}" />
-	 <Route path="DisplayParkings" component="{DisplayParkings}" />
-	 <Route path="BookingDetails" component="{BookingDetails}" />
-	</div>
-  </Router>
+	import { onMount } from 'svelte';
+	import Login from "./components/Login.svelte"
+	import Home from "./components/Home.svelte"
+	import { Router, Link, Route } from 'svelte-routing';
+	import Nav from "./components/Nav.svelte";
+	import Nav2 from "./components/Nav2.svelte";
+	import Footer from './components/footer.svelte';
+	import DisplayParkings from './components/DisplayParkings.svelte';
+	import BookingDetails from './components/BookingDetails.svelte';
+
+	
+</script>
+
+
+<main>
+<Router>
+
+	<Nav/>
+	
+		<Route path="/" component={Home} />
+		<Route path="login" component={Login} />
+		<Route path="DisplayParkings" component={DisplayParkings} />
+	 	<Route path="BookingDetails" component={BookingDetails} />
+
+	<Footer/>
+	
+</Router>
+</main>
+
--- client/src/components/Home.svelte
+++ client/src/components/Home.svelte
@@ -1,10 +1,68 @@
 <script>
+
+import axios from 'axios';
+import { onMount } from 'svelte';
+
 import CardParkingArea from "./card-parkingArea.svelte";
 import SearchBar from "./SearchBar.svelte";
+import { BaseUrl, allParkingAreas } from "../store/parkingdata.js"
 
+
+export let parkingsArray = [];
+
+
+onMount(async () => {
+    try {
+
+        const response = await fetch(`${BaseUrl}/api/parkingarea`, {
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+        });
+
+        const data = await response.json();
+        parkingsArray = data.ParkingAreas;
+        allParkingAreas.set(parkingsArray)
+
+        //console.log(parkingsArray);
+    
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+});
 
 </script>
 
 
+<style>
+    .container{
+        border: 2px solid;
+    }
+</style>
+
+
 <SearchBar/>
-<CardParkingArea/>
+
+
+<section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+    <div class="container">
+       <div class="flex flex-wrap -mx-4">
+
+{#if parkingsArray.length === 0}
+   <h1>Loading...</h1>
+{:else}
+ 
+    {#each parkingsArray as oneParkingArea} 
+        <CardParkingArea parkingAreaInfo = {oneParkingArea}/>
+   
+{/each} 
+        
+   
+
+ 
+{/if}
+
+</div>
+</div>
+</section>
