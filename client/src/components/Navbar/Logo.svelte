e0b06ed2aa3f762eadac292513706ca337078710
unknown <omar1807@stud.kea.dk>
2022-05-31 11:54:10 +0200

    updates fixes

--- client/src/components/Navbar/Logo.svelte
+++ client/src/components/Navbar/Logo.svelte
@@ -1,17 +1,4 @@
 <a href="/">
-<<<<<<< HEAD
-  <svg width=100% height=30>
-    <text class="font-parkingTitle" x=0 y=20>OAK PARKING</text>
-   
-  </svg>
-  </a>
-
-  <style>
-    text {
-      fill: black
-    }
-  </style>
-=======
 <svg width=100% height=30>
 	<text class="font-parkingTitle" x=0 y=20>OAK PARKING</text>
 </svg>
@@ -21,4 +8,3 @@
 		fill: black
 	}
 </style>
->>>>>>> 7c122259fe659356a5b4cbdc0beb812c77e4773e
--- client/src/components/Navbar/Menu.svelte
+++ client/src/components/Navbar/Menu.svelte
@@ -1,13 +1,4 @@
 <script>
-<<<<<<< HEAD
-  import { Router, Link, Route } from 'svelte-routing';
-  import {BaseUrl, authenticated, authenticatedUser} from "../../store/parkingdata.js";
-  import { logout } from "../../store/utils.js";
-
-  let auth = false;
-authenticated.subscribe(a => auth = a);
-
-=======
     import { Router, Link, Route } from 'svelte-routing';
     import {BaseUrl, authenticated, authenticatedUser} from "../../store/parkingdata.js";
     import { logout } from "../../store/utils.js";
@@ -15,24 +6,10 @@ authenticated.subscribe(a => auth = a);
     let auth = false;
 	authenticated.subscribe(a => auth = a);
 	
->>>>>>> 7c122259fe659356a5b4cbdc0beb812c77e4773e
 
 </script>
 
 <nav class="hidden text-gray-500 uppercase text-bold sm:block">
-<<<<<<< HEAD
-  
-      <Link to = "/" class="p-2 font-parkingTitle hover:text-red-900">Home</Link>
-      <Link to = "map" class="p-2 font-parkingTitle hover:text-red-900">Map</Link>
-      <Link to = "about" class="p-2 font-parkingTitle hover:text-red-900">About</Link>
-      <Link to = "contact" class="p-2 font-parkingTitle hover:text-red-900">Contact</Link>
-
-      {#if auth}
-          <Link to = "/" on:click={logout} class="p-5 font-parkingTitle hover:text-red-900">Log Out</Link>
-      {:else}
-          <Link to = "login" class="p-5 font-parkingTitle hover:text-red-900">Log in</Link>
-      {/if}
-=======
     
         <Link to = "/" class="p-2 font-parkingTitle hover:text-red-900">Home</Link>
         <Link to = "map" class="p-2 font-parkingTitle hover:text-red-900">Map</Link>
@@ -44,6 +21,5 @@ authenticated.subscribe(a => auth = a);
         {:else}
             <Link to = "login" class="p-5 font-parkingTitle hover:text-red-900">Log in</Link>
         {/if}
->>>>>>> 7c122259fe659356a5b4cbdc0beb812c77e4773e
 
 </nav>
