4b30942811965103ad3862bdbb909e60030bbb64
unknown <omar1807@stud.kea.dk>
2022-05-31 16:24:48 +0200

    added signup compoenents and links to login

--- client/src/routes/Signup.svelte
+++ client/src/routes/Signup.svelte
@@ -1,43 +1,106 @@
+<script>
+  import { BaseUrl } from "../store/parkingdata.js";
+
+  let firstName,
+    lastName,
+    email,
+    plateNumber,
+    password = "";
+
+  const signUp = async () => {
+    await fetch(`${BaseUrl}/api/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        email: email.toLowerCase(),
+        plateNumber: plateNumber,
+        password: password,
+      }),
+    });
+
+    location.replace("/login");
+  };
+</script>
+
 <!-- component -->
 <section class="py-26 bg-white">
   <div class="container px-4 mx-auto">
     <div class="max-w-lg mx-auto">
       <div class="text-center mb-8">
         <a class="inline-block mx-auto mb-6" href="#">
-          <img src="nigodo-assets/logo-icon-nigodo.svg" alt="">
+          <img src="nigodo-assets/logo-icon-nigodo.svg" alt="" />
         </a>
         <h2 class="text-3xl md:text-4xl font-extrabold mb-2">Sign up</h2>
-          </div>
-      <form action="">
-
+      </div>
+      <form on:submit|preventDefault={signUp}>
         <div class="mb-2">
           <label class="block mb-2 font-extrabold" for="">First name</label>
-          <input class="inline-block w-full p-4 leading-6 text-base font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded" type="text" placeholder="First name" required>
+          <input
+            bind:value={firstName}
+            class="inline-block w-full p-4 leading-6 text-base font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
+            type="text"
+            placeholder="First name"
+            required
+          />
         </div>
 
         <div class="mb-2">
           <label class="block mb-2 font-extrabold" for="">Last name</label>
-          <input class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded" type="text" placeholder="Last name" required>
+          <input
+            bind:value={lastName}
+            class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
+            type="text"
+            placeholder="Last name"
+            required
+          />
         </div>
 
         <div class="mb-2">
           <label class="block mb-2 font-extrabold" for="">Email</label>
-          <input class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded" type="email" placeholder="email" required>
+          <input
+            bind:value={email}
+            class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
+            type="email"
+            placeholder="email"
+            required
+          />
         </div>
 
         <div class="mb-2">
           <label class="block mb-2 font-extrabold" for="">Plate number</label>
-          <input class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded" type="text" placeholder="Plate number" required>
+          <input
+            bind:value={plateNumber}
+            class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
+            type="text"
+            placeholder="Plate number"
+            required
+          />
         </div>
 
         <div class="mb-5">
           <label class="block mb-2 font-extrabold" for="">Password</label>
-          <input class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded" type="password" required>
+          <input
+            bind:value={password}
+            class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
+            type="password"
+            required
+          />
         </div>
-        
-        <button class="inline-block w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-indigo-800 hover:bg-indigo-900 border-3 border-indigo-900 shadow rounded transition duration-200">Sign up</button>
-        <p class="text-center font-extrabold">Do have an account? <a class="text-red-500 hover:underline" href="login">Sign in</a></p>
+
+        <button
+          class="inline-block w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-indigo-800 hover:bg-indigo-900 border-3 border-indigo-900 shadow rounded transition duration-200"
+          >Sign up</button
+        >
+        <p class="text-center font-extrabold">
+          Do have an account? <a
+            class="text-red-500 hover:underline"
+            href="login">Sign in</a
+          >
+        </p>
       </form>
     </div>
   </div>
-</section><!-- component -->
+</section>
+<!-- component -->
