35793b45a973d3de5b869696836315157ba01844
Enachi Adrian <enachi.adrian52@gmail.com>
2022-06-21 19:20:07 +0200

    profile

--- client/src/routes/Profile.svelte
+++ client/src/routes/Profile.svelte
@@ -1,74 +1,4 @@
 <script>
-<<<<<<< HEAD
-
-import { allParkingAreas, authenticatedUser, selectedParkingArea } from "../store/parkingdata.js";
-
-let parkings = $allParkingAreas;
-
-console.log(parkings[0].slot);
-
-let firstName = $authenticatedUser.first_name
-let lastName = $authenticatedUser.last_name
-let email = $authenticatedUser.email
-let plateNumber = $authenticatedUser.plateNumber
-let balance = $authenticatedUser.balance
-
-
-
-
-let mySlots = []
-
-
-parkings.forEach((parking) => {
-    let parkingName = parking.name;
-    const slots = parking.slot
-    
-    slots.forEach((sl) => {
-        if(plateNumber === sl.plateNumber){
-             let userSlot = {parkingName: parkingName, slot: sl }
-             mySlots.push(userSlot)
-         }
-     })
-})
-            
-
-
-const refactorTime = (time) => {
-    return time
-}              
-    
-
-
-
-console.log(mySlots);
-
-
-</script>
-
-
-<h1>Profile</h1>
-<h3>{firstName}</h3>
-<h3>{lastName}</h3>
-<h3>{email}</h3>
-<h3>{plateNumber}</h3>
-<h3>{balance}</h3>
-
-<hr>
-
-
-{#each mySlots as oneSlot} 
-
-            <h1>{oneSlot.parkingName}</h1>
-            <p>{oneSlot.slot.number}</p>
-            <p>{refactorTime(oneSlot.slot.startTime)}</p>
-            <p>{refactorTime(oneSlot.slot.endTime)}</p>
-            <hr>
-             
-{/each} 
-
-<h1>Hello</h1>
-<hr>
-=======
 
     import { allParkingAreas, authenticatedUser, selectedParkingArea } from "../store/parkingdata.js";
     
@@ -142,4 +72,3 @@ console.log(mySlots);
     
     <h1>Hello</h1>
     <hr>
->>>>>>> e24e32d36a8ba0e3ea97cc3866a3b2b2f9c25a92
